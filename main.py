1a6d711570c7853f92e90d4e793c00ed4e3b703c
Mathis Boulais <https://github.com/Asukyy>
2023-04-06 18:41:13 +0200

    fin

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ cap = cv2.VideoCapture(0)
 x = 0
 w = 0
 
+# face et eye cascade avec le bon chemin en xml
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
 while True:
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) #1.3 = scale factor, 5 = min neighbors
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
@@ -22,11 +24,12 @@ while True:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
+#nombre de tete
     if x + w/2 < frame.shape[1] / 2:
         print('droite')
     else:
         print('gauche')
+
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
